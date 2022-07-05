8ceec42bc252489ac0ef3ff9d638910cdecd8fe1
Monika Tuzel <61913357+MonikaTuzel@users.noreply.github.com>
2022-07-05 10:08:16 +0200

    Initial commit

---
+++ Palidromy/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Palidromy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sprawdzenie spr = new Sprawdzenie();
+
+            Console.WriteLine("PALIDROMY");
+            Console.WriteLine("Podaj słowo do sprawdzenia: ");
+
+            string slowo = Console.ReadLine().ToUpper();
+
+            var czyPalidrom = spr.zamianaLiter(slowo);
+
+            if (czyPalidrom)
+                Console.WriteLine($"Podane słowo: {slowo}, jest palidromem!");
+            else
+                Console.WriteLine($"Podane słowo: {slowo}, nie jest palidromem");
+        }
+    }
+}
---
+++ Palidromy/Sprawdzenie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Palidromy
+{
+    internal class Sprawdzenie
+    {
+        public bool zamianaLiter(string slowo)
+        {
+            var podaneSlowo = slowo.ToCharArray();
+
+            char[] palidrom = new char[podaneSlowo.Length];
+
+            int iloscLiter = podaneSlowo.Length;
+
+            bool czyPalidrom = false;
+
+            foreach (var c in podaneSlowo)
+            {
+                palidrom[iloscLiter - 1] = c;
+                iloscLiter--;
+            }            
+
+            if (new string(podaneSlowo) == new string(palidrom))
+                czyPalidrom=true;
+            else
+                czyPalidrom=false;
+
+            return czyPalidrom;
+        }
+    }
+}
